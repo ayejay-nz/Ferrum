694ccbef591e2762fbf08cd9aa9bcafcd305c000
ayejay-nz <55215693+ayejay-nz@users.noreply.github.com>
2026-04-02 17:15:01 +1300

    Refactor piece-square tables to use `Score` type

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -21,156 +21,87 @@ const PIECE_VALUES: [PieceValues; 6] = [
     PieceValues { mg: 0, eg: 0 },      // King
 ];
 
-type PST = [Eval; 64];
+type PST = [Score; 64];
 
 #[rustfmt::skip]
-const MG_PAWN_PST: PST = [
-    0,   0,   0,   0,   0,   0,  0,   0,
-    98, 134,  61,  95,  68, 126, 34, -11,
-    -6,   7,  26,  31,  65,  56, 25, -20,
-    -14,  13,   6,  21,  23,  12, 17, -23,
-    -27,  -2,  -5,  12,  17,   6, 10, -25,
-    -26,  -4,  -4, -10,   3,   3, 33, -12,
-    -35,  -1, -20, -23, -15,  24, 38, -22,
-    0,   0,   0,   0,   0,   0,  0,   0,
-];
-#[rustfmt::skip]
-const MG_KNIGHT_PST: PST = [
-    -167, -89, -34, -49,  61, -97, -15, -107,
-    -73, -41,  72,  36,  23,  62,   7,  -17,
-    -47,  60,  37,  65,  84, 129,  73,   44,
-    -9,  17,  19,  53,  37,  69,  18,   22,
-    -13,   4,  16,  13,  28,  19,  21,   -8,
-    -23,  -9,  12,  10,  19,  17,  25,  -16,
-    -29, -53, -12,  -3,  -1,  18, -14,  -19,
-    -105, -21, -58, -33, -17, -28, -19,  -23,
-];
-#[rustfmt::skip]
-const MG_BISHOP_PST: PST = [
-    -29,   4, -82, -37, -25, -42,   7,  -8,
-    -26,  16, -18, -13,  30,  59,  18, -47,
-    -16,  37,  43,  40,  35,  50,  37,  -2,
-    -4,   5,  19,  50,  37,  37,   7,  -2,
-    -6,  13,  13,  26,  34,  12,  10,   4,
-    0,  15,  15,  15,  14,  27,  18,  10,
-    4,  15,  16,   0,   7,  21,  33,   1,
-    -33,  -3, -14, -21, -13, -12, -39, -21,
-];
-#[rustfmt::skip]
-const MG_ROOK_PST: PST = [
-    32,  42,  32,  51, 63,  9,  31,  43,
-    27,  32,  58,  62, 80, 67,  26,  44,
-    -5,  19,  26,  36, 17, 45,  61,  16,
-    -24, -11,   7,  26, 24, 35,  -8, -20,
-    -36, -26, -12,  -1,  9, -7,   6, -23,
-    -45, -25, -16, -17,  3,  0,  -5, -33,
-    -44, -16, -20,  -9, -1, 11,  -6, -71,
-    -19, -13,   1,  17, 16,  7, -37, -26,
-];
-#[rustfmt::skip]
-const MG_QUEEN_PST: PST = [
-    -28,   0,  29,  12,  59,  44,  43,  45,
-    -24, -39,  -5,   1, -16,  57,  28,  54,
-    -13, -17,   7,   8,  29,  56,  47,  57,
-    -27, -27, -16, -16,  -1,  17,  -2,   1,
-    -9, -26,  -9, -10,  -2,  -4,   3,  -3,
-    -14,   2, -11,  -2,  -5,   2,  14,   5,
-    -35,  -8,  11,   2,   8,  15,  -3,   1,
-    -1, -18,  -9,  10, -15, -25, -31, -50,
-];
-#[rustfmt::skip]
-const MG_KING_PST: PST = [
-    -65,  23,  16, -15, -56, -34,   2,  13,
-    29,  -1, -20,  -7,  -8,  -4, -38, -29,
-    -9,  24,   2, -16, -20,   6,  22, -22,
-    -17, -20, -12, -27, -30, -25, -14, -36,
-    -49,  -1, -27, -39, -46, -44, -33, -51,
-    -14, -14, -22, -46, -44, -30, -15, -27,
-    1,   7,  -8, -64, -43, -16,   9,   8,
-    -15,  36,  12, -54,   8, -28,  24,  14,
-];
-#[rustfmt::skip]
-const EG_PAWN_PST: PST = [
-    0,   0,   0,   0,   0,   0,   0,   0,
-    178, 173, 158, 134, 147, 132, 165, 187,
-    94, 100,  85,  67,  56,  53,  82,  84,
-    32,  24,  13,   5,  -2,   4,  17,  17,
-    13,   9,  -3,  -7,  -7,  -8,   3,  -1,
-    4,   7,  -6,   1,   0,  -5,  -1,  -8,
-    13,   8,   8,  10,  13,   0,   2,  -7,
-    0,   0,   0,   0,   0,   0,   0,   0,
-];
-#[rustfmt::skip]
-const EG_KNIGHT_PST: PST = [
-    -58, -38, -13, -28, -31, -27, -63, -99,
-    -25,  -8, -25,  -2,  -9, -25, -24, -52,
-    -24, -20,  10,   9,  -1,  -9, -19, -41,
-    -17,   3,  22,  22,  22,  11,   8, -18,
-    -18,  -6,  16,  25,  16,  17,   4, -18,
-    -23,  -3,  -1,  15,  10,  -3, -20, -22,
-    -42, -20, -10,  -5,  -2, -20, -23, -44,
-    -29, -51, -23, -15, -22, -18, -50, -64,
-];
-#[rustfmt::skip]
-const EG_BISHOP_PST: PST = [
-    -14, -21, -11,  -8, -7,  -9, -17, -24,
-    -8,  -4,   7, -12, -3, -13,  -4, -14,
-    2,  -8,   0,  -1, -2,   6,   0,   4,
-    -3,   9,  12,   9, 14,  10,   3,   2,
-    -6,   3,  13,  19,  7,  10,  -3,  -9,
-    -12,  -3,   8,  10, 13,   3,  -7, -15,
-    -14, -18,  -7,  -1,  4,  -9, -15, -27,
-    -23,  -9, -23,  -5, -9, -16,  -5, -17,
-];
-#[rustfmt::skip]
-const EG_ROOK_PST: PST = [
-    13, 10, 18, 15, 12,  12,   8,   5,
-    11, 13, 13, 11, -3,   3,   8,   3,
-    7,  7,  7,  5,  4,  -3,  -5,  -3,
-    4,  3, 13,  1,  2,   1,  -1,   2,
-    3,  5,  8,  4, -5,  -6,  -8, -11,
-    -4,  0, -5, -1, -7, -12,  -8, -16,
-    -6, -6,  0,  2, -9,  -9, -11,  -3,
-    -9,  2,  3, -1, -5, -13,   4, -20,
-];
-#[rustfmt::skip]
-const EG_QUEEN_PST: PST = [
-    -9,  22,  22,  27,  27,  19,  10,  20,
-    -17,  20,  32,  41,  58,  25,  30,   0,
-    -20,   6,   9,  49,  47,  35,  19,   9,
-    3,  22,  24,  45,  57,  40,  57,  36,
-    -18,  28,  19,  47,  31,  34,  39,  23,
-    -16, -27,  15,   6,   9,  17,  10,   5,
-    -22, -23, -30, -16, -16, -23, -36, -32,
-    -33, -28, -22, -43,  -5, -32, -20, -41,
-];
-#[rustfmt::skip]
-const EG_KING_PST: PST = [
-    -74, -35, -18, -18, -11,  15,   4, -17,
-    -12,  17,  14,  17,  17,  38,  23,  11,
-    10,  17,  23,  15,  20,  45,  44,  13,
-    -8,  22,  24,  27,  26,  33,  26,   3,
-    -18,  -4,  21,  24,  27,  23,   9, -11,
-    -19,  -3,  11,  21,  23,  16,   7,  -9,
-    -27, -11,   4,  13,  14,   4,  -5, -17,
-    -53, -34, -21, -11, -28, -14, -24, -43,
+const PAWN_PST: PST = [
+    Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 },
+    Score { mg:   98, eg:  178 }, Score { mg:  134, eg:  173 }, Score { mg:   61, eg:  158 }, Score { mg:   95, eg:  134 }, Score { mg:   68, eg:  147 }, Score { mg:  126, eg:  132 }, Score { mg:   34, eg:  165 }, Score { mg:  -11, eg:  187 },
+    Score { mg:   -6, eg:   94 }, Score { mg:    7, eg:  100 }, Score { mg:   26, eg:   85 }, Score { mg:   31, eg:   67 }, Score { mg:   65, eg:   56 }, Score { mg:   56, eg:   53 }, Score { mg:   25, eg:   82 }, Score { mg:  -20, eg:   84 },
+    Score { mg:  -14, eg:   32 }, Score { mg:   13, eg:   24 }, Score { mg:    6, eg:   13 }, Score { mg:   21, eg:    5 }, Score { mg:   23, eg:   -2 }, Score { mg:   12, eg:    4 }, Score { mg:   17, eg:   17 }, Score { mg:  -23, eg:   17 },
+    Score { mg:  -27, eg:   13 }, Score { mg:   -2, eg:    9 }, Score { mg:   -5, eg:   -3 }, Score { mg:   12, eg:   -7 }, Score { mg:   17, eg:   -7 }, Score { mg:    6, eg:   -8 }, Score { mg:   10, eg:    3 }, Score { mg:  -25, eg:   -1 },
+    Score { mg:  -26, eg:    4 }, Score { mg:   -4, eg:    7 }, Score { mg:   -4, eg:   -6 }, Score { mg:  -10, eg:    1 }, Score { mg:    3, eg:    0 }, Score { mg:    3, eg:   -5 }, Score { mg:   33, eg:   -1 }, Score { mg:  -12, eg:   -8 },
+    Score { mg:  -35, eg:   13 }, Score { mg:   -1, eg:    8 }, Score { mg:  -20, eg:    8 }, Score { mg:  -23, eg:   10 }, Score { mg:  -15, eg:   13 }, Score { mg:   24, eg:    0 }, Score { mg:   38, eg:    2 }, Score { mg:  -22, eg:   -7 },
+    Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }, Score { mg:    0, eg:    0 }
 ];
 
-const MG_PST: [PST; 6] = [
-    MG_PAWN_PST,
-    MG_KNIGHT_PST,
-    MG_BISHOP_PST,
-    MG_ROOK_PST,
-    MG_QUEEN_PST,
-    MG_KING_PST,
+#[rustfmt::skip]
+const KNIGHT_PST: PST = [
+    Score { mg: -167, eg:  -58 }, Score { mg:  -89, eg:  -38 }, Score { mg:  -34, eg:  -13 }, Score { mg:  -49, eg:  -28 }, Score { mg:   61, eg:  -31 }, Score { mg:  -97, eg:  -27 }, Score { mg:  -15, eg:  -63 }, Score { mg: -107, eg:  -99 },
+    Score { mg:  -73, eg:  -25 }, Score { mg:  -41, eg:   -8 }, Score { mg:   72, eg:  -25 }, Score { mg:   36, eg:   -2 }, Score { mg:   23, eg:   -9 }, Score { mg:   62, eg:  -25 }, Score { mg:    7, eg:  -24 }, Score { mg:  -17, eg:  -52 },
+    Score { mg:  -47, eg:  -24 }, Score { mg:   60, eg:  -20 }, Score { mg:   37, eg:   10 }, Score { mg:   65, eg:    9 }, Score { mg:   84, eg:   -1 }, Score { mg:  129, eg:   -9 }, Score { mg:   73, eg:  -19 }, Score { mg:   44, eg:  -41 },
+    Score { mg:   -9, eg:  -17 }, Score { mg:   17, eg:    3 }, Score { mg:   19, eg:   22 }, Score { mg:   53, eg:   22 }, Score { mg:   37, eg:   22 }, Score { mg:   69, eg:   11 }, Score { mg:   18, eg:    8 }, Score { mg:   22, eg:  -18 },
+    Score { mg:  -13, eg:  -18 }, Score { mg:    4, eg:   -6 }, Score { mg:   16, eg:   16 }, Score { mg:   13, eg:   25 }, Score { mg:   28, eg:   16 }, Score { mg:   19, eg:   17 }, Score { mg:   21, eg:    4 }, Score { mg:   -8, eg:  -18 },
+    Score { mg:  -23, eg:  -23 }, Score { mg:   -9, eg:   -3 }, Score { mg:   12, eg:   -1 }, Score { mg:   10, eg:   15 }, Score { mg:   19, eg:   10 }, Score { mg:   17, eg:   -3 }, Score { mg:   25, eg:  -20 }, Score { mg:  -16, eg:  -22 },
+    Score { mg:  -29, eg:  -42 }, Score { mg:  -53, eg:  -20 }, Score { mg:  -12, eg:  -10 }, Score { mg:   -3, eg:   -5 }, Score { mg:   -1, eg:   -2 }, Score { mg:   18, eg:  -20 }, Score { mg:  -14, eg:  -23 }, Score { mg:  -19, eg:  -44 },
+    Score { mg: -105, eg:  -29 }, Score { mg:  -21, eg:  -51 }, Score { mg:  -58, eg:  -23 }, Score { mg:  -33, eg:  -15 }, Score { mg:  -17, eg:  -22 }, Score { mg:  -28, eg:  -18 }, Score { mg:  -19, eg:  -50 }, Score { mg:  -23, eg:  -64 }
 ];
-const EG_PST: [PST; 6] = [
-    EG_PAWN_PST,
-    EG_KNIGHT_PST,
-    EG_BISHOP_PST,
-    EG_ROOK_PST,
-    EG_QUEEN_PST,
-    EG_KING_PST,
+
+#[rustfmt::skip]
+const BISHOP_PST: PST = [
+    Score { mg:  -29, eg:  -14 }, Score { mg:    4, eg:  -21 }, Score { mg:  -82, eg:  -11 }, Score { mg:  -37, eg:   -8 }, Score { mg:  -25, eg:   -7 }, Score { mg:  -42, eg:   -9 }, Score { mg:    7, eg:  -17 }, Score { mg:   -8, eg:  -24 },
+    Score { mg:  -26, eg:   -8 }, Score { mg:   16, eg:   -4 }, Score { mg:  -18, eg:    7 }, Score { mg:  -13, eg:  -12 }, Score { mg:   30, eg:   -3 }, Score { mg:   59, eg:  -13 }, Score { mg:   18, eg:   -4 }, Score { mg:  -47, eg:  -14 },
+    Score { mg:  -16, eg:    2 }, Score { mg:   37, eg:   -8 }, Score { mg:   43, eg:    0 }, Score { mg:   40, eg:   -1 }, Score { mg:   35, eg:   -2 }, Score { mg:   50, eg:    6 }, Score { mg:   37, eg:    0 }, Score { mg:   -2, eg:    4 },
+    Score { mg:   -4, eg:   -3 }, Score { mg:    5, eg:    9 }, Score { mg:   19, eg:   12 }, Score { mg:   50, eg:    9 }, Score { mg:   37, eg:   14 }, Score { mg:   37, eg:   10 }, Score { mg:    7, eg:    3 }, Score { mg:   -2, eg:    2 },
+    Score { mg:   -6, eg:   -6 }, Score { mg:   13, eg:    3 }, Score { mg:   13, eg:   13 }, Score { mg:   26, eg:   19 }, Score { mg:   34, eg:    7 }, Score { mg:   12, eg:   10 }, Score { mg:   10, eg:   -3 }, Score { mg:    4, eg:   -9 },
+    Score { mg:    0, eg:  -12 }, Score { mg:   15, eg:   -3 }, Score { mg:   15, eg:    8 }, Score { mg:   15, eg:   10 }, Score { mg:   14, eg:   13 }, Score { mg:   27, eg:    3 }, Score { mg:   18, eg:   -7 }, Score { mg:   10, eg:  -15 },
+    Score { mg:    4, eg:  -14 }, Score { mg:   15, eg:  -18 }, Score { mg:   16, eg:   -7 }, Score { mg:    0, eg:   -1 }, Score { mg:    7, eg:    4 }, Score { mg:   21, eg:   -9 }, Score { mg:   33, eg:  -15 }, Score { mg:    1, eg:  -27 },
+    Score { mg:  -33, eg:  -23 }, Score { mg:   -3, eg:   -9 }, Score { mg:  -14, eg:  -23 }, Score { mg:  -21, eg:   -5 }, Score { mg:  -13, eg:   -9 }, Score { mg:  -12, eg:  -16 }, Score { mg:  -39, eg:   -5 }, Score { mg:  -21, eg:  -17 }
+];
+
+#[rustfmt::skip]
+const ROOK_PST: PST = [
+    Score { mg:   32, eg:   13 }, Score { mg:   42, eg:   10 }, Score { mg:   32, eg:   18 }, Score { mg:   51, eg:   15 }, Score { mg:   63, eg:   12 }, Score { mg:    9, eg:   12 }, Score { mg:   31, eg:    8 }, Score { mg:   43, eg:    5 },
+    Score { mg:   27, eg:   11 }, Score { mg:   32, eg:   13 }, Score { mg:   58, eg:   13 }, Score { mg:   62, eg:   11 }, Score { mg:   80, eg:   -3 }, Score { mg:   67, eg:    3 }, Score { mg:   26, eg:    8 }, Score { mg:   44, eg:    3 },
+    Score { mg:   -5, eg:    7 }, Score { mg:   19, eg:    7 }, Score { mg:   26, eg:    7 }, Score { mg:   36, eg:    5 }, Score { mg:   17, eg:    4 }, Score { mg:   45, eg:   -3 }, Score { mg:   61, eg:   -5 }, Score { mg:   16, eg:   -3 },
+    Score { mg:  -24, eg:    4 }, Score { mg:  -11, eg:    3 }, Score { mg:    7, eg:   13 }, Score { mg:   26, eg:    1 }, Score { mg:   24, eg:    2 }, Score { mg:   35, eg:    1 }, Score { mg:   -8, eg:   -1 }, Score { mg:  -20, eg:    2 },
+    Score { mg:  -36, eg:    3 }, Score { mg:  -26, eg:    5 }, Score { mg:  -12, eg:    8 }, Score { mg:   -1, eg:    4 }, Score { mg:    9, eg:   -5 }, Score { mg:   -7, eg:   -6 }, Score { mg:    6, eg:   -8 }, Score { mg:  -23, eg:  -11 },
+    Score { mg:  -45, eg:   -4 }, Score { mg:  -25, eg:    0 }, Score { mg:  -16, eg:   -5 }, Score { mg:  -17, eg:   -1 }, Score { mg:    3, eg:   -7 }, Score { mg:    0, eg:  -12 }, Score { mg:   -5, eg:   -8 }, Score { mg:  -33, eg:  -16 },
+    Score { mg:  -44, eg:   -6 }, Score { mg:  -16, eg:   -6 }, Score { mg:  -20, eg:    0 }, Score { mg:   -9, eg:    2 }, Score { mg:   -1, eg:   -9 }, Score { mg:   11, eg:   -9 }, Score { mg:   -6, eg:  -11 }, Score { mg:  -71, eg:   -3 },
+    Score { mg:  -19, eg:   -9 }, Score { mg:  -13, eg:    2 }, Score { mg:    1, eg:    3 }, Score { mg:   17, eg:   -1 }, Score { mg:   16, eg:   -5 }, Score { mg:    7, eg:  -13 }, Score { mg:  -37, eg:    4 }, Score { mg:  -26, eg:  -20 }
+];
+
+#[rustfmt::skip]
+const QUEEN_PST: PST = [
+    Score { mg:  -28, eg:   -9 }, Score { mg:    0, eg:   22 }, Score { mg:   29, eg:   22 }, Score { mg:   12, eg:   27 }, Score { mg:   59, eg:   27 }, Score { mg:   44, eg:   19 }, Score { mg:   43, eg:   10 }, Score { mg:   45, eg:   20 },
+    Score { mg:  -24, eg:  -17 }, Score { mg:  -39, eg:   20 }, Score { mg:   -5, eg:   32 }, Score { mg:    1, eg:   41 }, Score { mg:  -16, eg:   58 }, Score { mg:   57, eg:   25 }, Score { mg:   28, eg:   30 }, Score { mg:   54, eg:    0 },
+    Score { mg:  -13, eg:  -20 }, Score { mg:  -17, eg:    6 }, Score { mg:    7, eg:    9 }, Score { mg:    8, eg:   49 }, Score { mg:   29, eg:   47 }, Score { mg:   56, eg:   35 }, Score { mg:   47, eg:   19 }, Score { mg:   57, eg:    9 },
+    Score { mg:  -27, eg:    3 }, Score { mg:  -27, eg:   22 }, Score { mg:  -16, eg:   24 }, Score { mg:  -16, eg:   45 }, Score { mg:   -1, eg:   57 }, Score { mg:   17, eg:   40 }, Score { mg:   -2, eg:   57 }, Score { mg:    1, eg:   36 },
+    Score { mg:   -9, eg:  -18 }, Score { mg:  -26, eg:   28 }, Score { mg:   -9, eg:   19 }, Score { mg:  -10, eg:   47 }, Score { mg:   -2, eg:   31 }, Score { mg:   -4, eg:   34 }, Score { mg:    3, eg:   39 }, Score { mg:   -3, eg:   23 },
+    Score { mg:  -14, eg:  -16 }, Score { mg:    2, eg:  -27 }, Score { mg:  -11, eg:   15 }, Score { mg:   -2, eg:    6 }, Score { mg:   -5, eg:    9 }, Score { mg:    2, eg:   17 }, Score { mg:   14, eg:   10 }, Score { mg:    5, eg:    5 },
+    Score { mg:  -35, eg:  -22 }, Score { mg:   -8, eg:  -23 }, Score { mg:   11, eg:  -30 }, Score { mg:    2, eg:  -16 }, Score { mg:    8, eg:  -16 }, Score { mg:   15, eg:  -23 }, Score { mg:   -3, eg:  -36 }, Score { mg:    1, eg:  -32 },
+    Score { mg:   -1, eg:  -33 }, Score { mg:  -18, eg:  -28 }, Score { mg:   -9, eg:  -22 }, Score { mg:   10, eg:  -43 }, Score { mg:  -15, eg:   -5 }, Score { mg:  -25, eg:  -32 }, Score { mg:  -31, eg:  -20 }, Score { mg:  -50, eg:  -41 }
+];
+
+#[rustfmt::skip]
+const KING_PST: PST = [
+    Score { mg:  -65, eg:  -74 }, Score { mg:   23, eg:  -35 }, Score { mg:   16, eg:  -18 }, Score { mg:  -15, eg:  -18 }, Score { mg:  -56, eg:  -11 }, Score { mg:  -34, eg:   15 }, Score { mg:    2, eg:    4 }, Score { mg:   13, eg:  -17 },
+    Score { mg:   29, eg:  -12 }, Score { mg:   -1, eg:   17 }, Score { mg:  -20, eg:   14 }, Score { mg:   -7, eg:   17 }, Score { mg:   -8, eg:   17 }, Score { mg:   -4, eg:   38 }, Score { mg:  -38, eg:   23 }, Score { mg:  -29, eg:   11 },
+    Score { mg:   -9, eg:   10 }, Score { mg:   24, eg:   17 }, Score { mg:    2, eg:   23 }, Score { mg:  -16, eg:   15 }, Score { mg:  -20, eg:   20 }, Score { mg:    6, eg:   45 }, Score { mg:   22, eg:   44 }, Score { mg:  -22, eg:   13 },
+    Score { mg:  -17, eg:   -8 }, Score { mg:  -20, eg:   22 }, Score { mg:  -12, eg:   24 }, Score { mg:  -27, eg:   27 }, Score { mg:  -30, eg:   26 }, Score { mg:  -25, eg:   33 }, Score { mg:  -14, eg:   26 }, Score { mg:  -36, eg:    3 },
+    Score { mg:  -49, eg:  -18 }, Score { mg:   -1, eg:   -4 }, Score { mg:  -27, eg:   21 }, Score { mg:  -39, eg:   24 }, Score { mg:  -46, eg:   27 }, Score { mg:  -44, eg:   23 }, Score { mg:  -33, eg:    9 }, Score { mg:  -51, eg:  -11 },
+    Score { mg:  -14, eg:  -19 }, Score { mg:  -14, eg:   -3 }, Score { mg:  -22, eg:   11 }, Score { mg:  -46, eg:   21 }, Score { mg:  -44, eg:   23 }, Score { mg:  -30, eg:   16 }, Score { mg:  -15, eg:    7 }, Score { mg:  -27, eg:   -9 },
+    Score { mg:    1, eg:  -27 }, Score { mg:    7, eg:  -11 }, Score { mg:   -8, eg:    4 }, Score { mg:  -64, eg:   13 }, Score { mg:  -43, eg:   14 }, Score { mg:  -16, eg:    4 }, Score { mg:    9, eg:   -5 }, Score { mg:    8, eg:  -17 },
+    Score { mg:  -15, eg:  -53 }, Score { mg:   36, eg:  -34 }, Score { mg:   12, eg:  -21 }, Score { mg:  -54, eg:  -11 }, Score { mg:    8, eg:  -28 }, Score { mg:  -28, eg:  -14 }, Score { mg:   24, eg:  -24 }, Score { mg:   14, eg:  -43 }
+];
+
+const PSTS: [PST; 6] = [
+    PAWN_PST,
+    KNIGHT_PST,
+    BISHOP_PST,
+    ROOK_PST,
+    QUEEN_PST,
+    KING_PST,
 ];
 
 const PHASE_WEIGHTS: [i32; 6] = [0, 1, 1, 2, 4, 0];
@@ -227,8 +158,8 @@ fn eval_material_pst(pos: &Position, score: &mut Score) -> i32 {
                 let lsb = bb.pop_lsb();
                 let sq = relative_square(colour, lsb);
 
-                score.mg += sign * (values.mg + MG_PST[piece.idx()][sq]);
-                score.eg += sign * (values.eg + EG_PST[piece.idx()][sq]);
+                score.mg += sign * (values.mg + PSTS[piece.idx()][sq].mg);
+                score.eg += sign * (values.eg + PSTS[piece.idx()][sq].eg);
 
                 phase += phase_weight(piece);
             }
